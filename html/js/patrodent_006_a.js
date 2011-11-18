52e86a351846c73995343312fe83d644602977d8
Pablo Martín <goinnn@gmail.com>
2011-11-18 20:40:50 +0000

    Modifico los js y los html con las nuevas rutas

--- html/js/patrodent_006_a.js
+++ html/js/patrodent_006_a.js
@@ -5,8 +5,8 @@ function OnWeLoad()
 {
 
 	Img16={max:1,pos:0};
-	Img16[0]=new Image();Img16[0].src='b-norm2.gif';
-	Img16[1]=new Image();Img16[1].src='b-norm.gif';	IDP.my=(window.parent.frames.length && parent.MyFrm)?parent.MyFrm:window;
+	Img16[0]=new Image();Img16[0].src='./images/b-norm2.gif';
+	Img16[1]=new Image();Img16[1].src='./images/b-norm.gif';	IDP.my=(window.parent.frames.length && parent.MyFrm)?parent.MyFrm:window;
 	IDP[1]=(V5)?'document.getElementById(\'e16\').src':(IE)?'e16.src':'document.e16.src';
 	isOvr=1;
 }
