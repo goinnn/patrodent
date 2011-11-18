85f7440a07038fec876c3b7f81ff3ee7f9b9a072
goinnn <goinnn@7d7dbc68-9661-6f5e-b39f-2f914dcb8a99>
2011-11-18 20:40:50 +0000

    Modifico los js y los html con las nuevas rutas

--- trunk/html/js/patrodent_005_a.js
+++ trunk/html/js/patrodent_005_a.js
@@ -5,23 +5,23 @@ function OnWeLoad()
 {
 
 	Img7={max:1,pos:0};
-	Img7[0]=new Image();Img7[0].src='b-norm2.gif';
-	Img7[1]=new Image();Img7[1].src='b-norm.gif';
+	Img7[0]=new Image();Img7[0].src='./images/b-norm2.gif';
+	Img7[1]=new Image();Img7[1].src='./images/b-norm.gif';
 	Img8={max:1,pos:0};
-	Img8[0]=new Image();Img8[0].src='b-norm2.gif';
-	Img8[1]=new Image();Img8[1].src='b-norm.gif';
+	Img8[0]=new Image();Img8[0].src='./images/b-norm2.gif';
+	Img8[1]=new Image();Img8[1].src='./images/b-norm.gif';
 	Img12={max:1,pos:0};
-	Img12[0]=new Image();Img12[0].src='b-norm2.gif';
-	Img12[1]=new Image();Img12[1].src='b-norm.gif';
+	Img12[0]=new Image();Img12[0].src='./images/b-norm2.gif';
+	Img12[1]=new Image();Img12[1].src='./images/b-norm.gif';
 	Img13={max:1,pos:0};
-	Img13[0]=new Image();Img13[0].src='b-norm2.gif';
-	Img13[1]=new Image();Img13[1].src='b-norm.gif';
+	Img13[0]=new Image();Img13[0].src='./images/b-norm2.gif';
+	Img13[1]=new Image();Img13[1].src='./images/b-norm.gif';
 	Img14={max:1,pos:0};
-	Img14[0]=new Image();Img14[0].src='b-norm2.gif';
-	Img14[1]=new Image();Img14[1].src='b-norm.gif';
+	Img14[0]=new Image();Img14[0].src='./images/b-norm2.gif';
+	Img14[1]=new Image();Img14[1].src='./images/b-norm.gif';
 	Img19={max:1,pos:0};
-	Img19[0]=new Image();Img19[0].src='b-norm2.gif';
-	Img19[1]=new Image();Img19[1].src='b-norm.gif';	IDP.my=(window.parent.frames.length && parent.MyFrm)?parent.MyFrm:window;
+	Img19[0]=new Image();Img19[0].src='./images/b-norm2.gif';
+	Img19[1]=new Image();Img19[1].src='./images/b-norm.gif';	IDP.my=(window.parent.frames.length && parent.MyFrm)?parent.MyFrm:window;
 	IDP[1]=(V5)?'document.getElementById(\'e7\').src':(IE)?'e7.src':'document.e7.src';
 	IDP[2]=(V5)?'document.getElementById(\'e8\').src':(IE)?'e8.src':'document.e8.src';
 	IDP[3]=(V5)?'document.getElementById(\'e12\').src':(IE)?'e12.src':'document.e12.src';
--- trunk/html/js/patrodent_010_a.js
+++ trunk/html/js/patrodent_010_a.js
@@ -5,8 +5,8 @@ function OnWeLoad()
 {
 
 	Img12={max:1,pos:0};
-	Img12[0]=new Image();Img12[0].src='b-norm2.gif';
-	Img12[1]=new Image();Img12[1].src='b-norm.gif';	IDP.my=(window.parent.frames.length && parent.MyFrm)?parent.MyFrm:window;
+	Img12[0]=new Image();Img12[0].src='./images/b-norm2.gif';
+	Img12[1]=new Image();Img12[1].src='./images/b-norm.gif';	IDP.my=(window.parent.frames.length && parent.MyFrm)?parent.MyFrm:window;
 	IDP[1]=(V5)?'document.getElementById(\'e12\').src':(IE)?'e12.src':'document.e12.src';
 	isOvr=1;
 }
