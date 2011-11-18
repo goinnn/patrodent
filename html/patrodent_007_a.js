fa49827e6e6ca2155878a9004970155545b126ef
Pablo Martín <goinnn@gmail.com>
2011-11-18 19:04:32 +0000

    Añado version inicial del proyecto

---
+++ html/patrodent_007_a.js
@@ -0,0 +1,15 @@
+/*--- Avanquest WebEasy Motion Script ---*/
+
+
+function OnWeLoad()
+{
+
+	Img11={max:1,pos:0};
+	Img11[0]=new Image();Img11[0].src='b-norm2.gif';
+	Img11[1]=new Image();Img11[1].src='b-norm.gif';	IDP.my=(window.parent.frames.length && parent.MyFrm)?parent.MyFrm:window;
+	IDP[1]=(V5)?'document.getElementById(\'e11\').src':(IE)?'e11.src':'document.e11.src';
+	isOvr=1;
+}
+
+
+/*--- EndOfFile ---*/
