9df8bab41d9d142cb4692d869b98b53485a195b7
goinnn <goinnn@7d7dbc68-9661-6f5e-b39f-2f914dcb8a99>
2011-11-18 19:04:32 +0000

    Añado version inicial del proyecto

---
+++ trunk/html/patrodent_005_a.js
@@ -0,0 +1,35 @@
+/*--- Avanquest WebEasy Motion Script ---*/
+
+
+function OnWeLoad()
+{
+
+	Img7={max:1,pos:0};
+	Img7[0]=new Image();Img7[0].src='b-norm2.gif';
+	Img7[1]=new Image();Img7[1].src='b-norm.gif';
+	Img8={max:1,pos:0};
+	Img8[0]=new Image();Img8[0].src='b-norm2.gif';
+	Img8[1]=new Image();Img8[1].src='b-norm.gif';
+	Img12={max:1,pos:0};
+	Img12[0]=new Image();Img12[0].src='b-norm2.gif';
+	Img12[1]=new Image();Img12[1].src='b-norm.gif';
+	Img13={max:1,pos:0};
+	Img13[0]=new Image();Img13[0].src='b-norm2.gif';
+	Img13[1]=new Image();Img13[1].src='b-norm.gif';
+	Img14={max:1,pos:0};
+	Img14[0]=new Image();Img14[0].src='b-norm2.gif';
+	Img14[1]=new Image();Img14[1].src='b-norm.gif';
+	Img19={max:1,pos:0};
+	Img19[0]=new Image();Img19[0].src='b-norm2.gif';
+	Img19[1]=new Image();Img19[1].src='b-norm.gif';	IDP.my=(window.parent.frames.length && parent.MyFrm)?parent.MyFrm:window;
+	IDP[1]=(V5)?'document.getElementById(\'e7\').src':(IE)?'e7.src':'document.e7.src';
+	IDP[2]=(V5)?'document.getElementById(\'e8\').src':(IE)?'e8.src':'document.e8.src';
+	IDP[3]=(V5)?'document.getElementById(\'e12\').src':(IE)?'e12.src':'document.e12.src';
+	IDP[4]=(V5)?'document.getElementById(\'e13\').src':(IE)?'e13.src':'document.e13.src';
+	IDP[5]=(V5)?'document.getElementById(\'e14\').src':(IE)?'e14.src':'document.e14.src';
+	IDP[6]=(V5)?'document.getElementById(\'e19\').src':(IE)?'e19.src':'document.e19.src';
+	isOvr=1;
+}
+
+
+/*--- EndOfFile ---*/
---
+++ trunk/html/patrodent_008_a.js
@@ -0,0 +1,15 @@
+/*--- Avanquest WebEasy Motion Script ---*/
+
+
+function OnWeLoad()
+{
+
+	Img10={max:1,pos:0};
+	Img10[0]=new Image();Img10[0].src='b-norm2.gif';
+	Img10[1]=new Image();Img10[1].src='b-norm.gif';	IDP.my=(window.parent.frames.length && parent.MyFrm)?parent.MyFrm:window;
+	IDP[1]=(V5)?'document.getElementById(\'e10\').src':(IE)?'e10.src':'document.e10.src';
+	isOvr=1;
+}
+
+
+/*--- EndOfFile ---*/
