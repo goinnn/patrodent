bb5175b3929b4b3ff25f41338716249b1ee0c24a
goinnn <goinnn@7d7dbc68-9661-6f5e-b39f-2f914dcb8a99>
2013-01-19 15:45:37 +0000

    Cambio etiquetas html

--- trunk/v2/patrodent/static/js/cufon-replace.js
+++ trunk/v2/patrodent/static/js/cufon-replace.js
@@ -1,1 +1,1 @@
-Cufon.replace('h6, .sign, .button, .important-block .title, a .sign', { fontFamily: 'Angelina', hover:true });
+Cufon.replace('h3, .sign, .button, .important-block .title, a .sign', { fontFamily: 'Angelina', hover:true });
