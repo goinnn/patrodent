e3080fd820c711801d6625a1387bbad6968dbbb1
jorgecorrea <jorgecorreaherrero@gmail.com>
2020-02-28 18:28:35 +0100

    changes for patrodent

--- v2/patrodent/patrodent/settings_local.py
+++ v2/patrodent/patrodent/settings_local.py
@@ -2,5 +2,7 @@ from .settings import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '85.137.21.91', 'jorge.theysay.me']
+ALLOWED_HOSTS = ['localhost', '85.137.200.30', 'jorge.theysay.me', '127.0.0.1']
 STATIC_ROOT = '/home/jcorrea/proyectos/patrodent/static'
+
+INTERNAL_IPS = ALLOWED_HOSTS
