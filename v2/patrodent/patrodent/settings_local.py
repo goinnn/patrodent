ec84b19d7b39d2d4baa4e43513775c8d642c7e67
Pablo Martín <goinnn@gmail.com>
2020-05-30 15:40:37 +0200

    DEBUG = False en produccion

--- v2/patrodent/patrodent/settings_local.py
+++ v2/patrodent/patrodent/settings_local.py
@@ -1,6 +1,6 @@
 from .settings import *
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '85.137.200.30', 'jorge.theysay.me', '127.0.0.1', '192.168.0.20', '81.46.242.249']
 ALLOWED_HOSTS = ['*']
